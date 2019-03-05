b6110e1938fa7cca0a3d85ed7aa756c00c504b13
Aykut Asil <aykuttasil@gmail.com>
2019-03-05 20:58:35 +0300

    palindrome-number solition

---
+++ palindrome-number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace palindrome_number
+{
+    /* 
+        - if X's sign is '-' than return false
+        - get mod 10 to X
+        -       
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = IsPalindrome(161);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            var temp = x;
+            var signX = Math.Sign(x);
+            if (signX == -1) return false;
+
+            var reverseInt = 0;
+            // x = 345
+            while (true)
+            {
+                var q = x % 10; // 5
+                x = x / 10; // 34
+                reverseInt = reverseInt * 10 + q;
+                if (x == 0) break;
+            }
+
+            Console.WriteLine("temp:" + temp + " reverseInt:" + reverseInt);
+            if (temp == reverseInt) return true;
+            else return false;
+        }
+    }
+}
